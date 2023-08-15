88c0a148260a46c2b48d3af3352cdded839d6435
Malicious-dev <Programming1hars@gmail.com>
2023-08-15 11:32:06 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
